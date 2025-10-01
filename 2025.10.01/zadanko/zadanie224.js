dcd69310a03a859d119605538ed0146330f8b83e
Spring3r <145098382+Spring3r@users.noreply.github.com>
2025-10-01 08:22:39 +0200

    2025.10.01

---
+++ 2025.10.01/zadanko/zadanie224.js
@@ -0,0 +1,47 @@
+let http = require('http');
+const url=require("url");
+const fs=require('node:fs');
+http.createServer(function (req, res) {
+
+
+    switch (req.url.split('?')[0]) {
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Strona glowna');
+            break;
+        case '/2':
+            res.writeHead(200, {'Content-Type': 'text/JSON'});
+            const obj={name:"Maciej",surname:"Musial",age:36};
+            const myJSON=JSON.stringify(obj);
+            res.end(myJSON);
+            break;
+        case '/3':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1 style="color: darkgreen">Dzien dobry</h1>');
+            break;
+        case '/4':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+
+            const data= fs.readFileSync('C:\\Users\\student\\Desktop\\pliczek.html','utf8');
+            res.end(data);
+            break;
+        case '/get_params':
+            res.writeHead(200, {'Content-Type': 'text/JSON'});
+            const ParsedUrl=url.parse(req.url,true);
+            console.log(ParsedUrl);
+            const query=ParsedUrl.query;
+            const message={ok:"ok"};
+            const paramsArray=JSON.stringify(query);
+            const timestamp=Date.now();
+            const name=`params_${timestamp}.json`;
+            fs.writeFileSync(name,paramsArray,"utf8");
+            console.log(paramsArray);
+            res.end(JSON.stringify(message));
+            break;
+        default:
+            res.end("Zly adres");
+            break;
+    }
+
+    res.end();
+}).listen(8080);
